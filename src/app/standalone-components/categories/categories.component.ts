88ef98f92a37b967132ebb15a30874996e29f23f
unknown <saharevishal41@gmail.com>
2023-05-15 19:21:20 +0530

    when ever css class transfet to parent to chiel then its not apply at that time encapsulation is used (.Emulated- its defalt which not transfer parent to chaeld) ==(.none - this is apply all where its not remove any css)==(.Shadow - its remover chield css and apply own parent css )

--- src/app/standalone-components/categories/categories.component.ts
+++ src/app/standalone-components/categories/categories.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component,OnInit, ViewEncapsulation } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReusableTableComponent } from '../reusable-table/reusable-table.component';
 import { CategiriesService } from 'src/app/config/categiries.service';
@@ -9,7 +9,8 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [CommonModule,ReusableTableComponent],
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class CategoriesComponent {
 public categoriesData:any[]=[]
